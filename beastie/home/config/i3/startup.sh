ffbc0f70be62b9aa706b1c860fc3c52dcbc569a3
Ata Niyazov <ata.niazov@gmail.com>
2016-11-23 14:14:10 +0300

    Some fixes

--- beastie/home/config/i3/startup.sh
+++ beastie/home/config/i3/startup.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 # Kill StartUp daemons
 pkill xfsettingsd
+pkill xfce4-power-manager
+pkill xfce4-volumed
 pkill redshift
-#pkill xfce4-power-manager
-#pkill xfce4-volumed
 sleep 0.5
 # StartUp daemons
 xfsettingsd &
+xfce4-power-manager &
+xfce4-volumed &
 redshift -l 40.7:29.9 -t 5700:5000 -g 0.8 -m randr &
-#xfce4-power-manager &
-#xfce4-volumed &
