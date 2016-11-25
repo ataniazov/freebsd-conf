014ec43cb55b656d078e1ce5bcc6ece08b7f5dc1
Ata Niyazov <ata.niazov@gmail.com>
2016-11-25 09:56:17 +0300

    Replaces my_i3status.sh with i3statusTitle.sh

--- beastie/home/config/i3/startup.sh
+++ beastie/home/config/i3/startup.sh
@@ -5,10 +5,12 @@ pkill xfce4-power-manager
 pkill xfce4-volumed
 pkill redshift
 pkill compton
+#pkill conky
 sleep 0.5
 # StartUp daemons
 xfsettingsd &
 xfce4-power-manager &
 xfce4-volumed &
 redshift -l 40.7:29.9 -t 5900:5500 -g 0.8 -m randr &
-compton -c --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.1 --dbus &
+compton -c --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.1 --dbus --shadow-exclude fullscreen &
+#conky -c "/home/delta/.conky/SimpleClock/DArk_Simple_" &
