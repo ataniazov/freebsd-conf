04a9675eb973ea1b9eaa3a348a42ae00513a6c0a
Ata Niyazov <ata.niazov@gmail.com>
2016-11-25 11:03:52 +0300

    Improve i3statusTitle.sh

--- beastie/home/config/i3status/i3statusTitle.sh
+++ beastie/home/config/i3status/i3statusTitle.sh
@@ -5,7 +5,11 @@ i3status | while :
 do
 	read line
 	#echo "mystuff | $line" || exit 1
-	name=$(xdotool getactivewindow getwindowname 2>/dev/null || \
+	title=$(xdotool getactivewindow getwindowname 2>/dev/null || \
 		echo Workspace `i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2`)
-	echo "$name | $line" || exit 1
+	if [ ${#title} -ge 100 ]; then
+		echo "${title##*-} | $line" || exit 1
+	else
+		echo "$title | $line" || exit 1
+	fi
 done
