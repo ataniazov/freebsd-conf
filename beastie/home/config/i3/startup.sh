d5eac66f13573fd7aa8a7a83a58a6aabe4c994e1
Ata Niyazov <ata.niazov@gmail.com>
2016-11-28 19:43:41 +0300

    Some fixes

--- beastie/home/config/i3/startup.sh
+++ beastie/home/config/i3/startup.sh
@@ -6,11 +6,11 @@ pkill xfce4-volumed
 pkill redshift
 pkill compton
 #pkill conky
-sleep 0.5
+sleep 1
 # StartUp daemons
 xfsettingsd &
 xfce4-power-manager &
 xfce4-volumed &
 redshift -l 40.7:29.9 -t 5900:5500 -g 0.8 -m randr &
-compton -c --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.1 --dbus --shadow-exclude fullscreen &
+compton -cC --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.1 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl-mswc --shadow-exclude fullscreen &
 #conky -c "/home/delta/.conky/SimpleClock/DArk_Simple_" &
--- beastie/home/config/i3status/i3statusTitle.sh
+++ beastie/home/config/i3status/i3statusTitle.sh
@@ -7,7 +7,7 @@ do
 	#echo "mystuff | $line" || exit 1
 	title=$(xdotool getactivewindow getwindowname 2>/dev/null || \
 		echo Workspace `i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2`)
-	if [ ${#title} -ge 100 ]; then
+	if [ ${#title} -ge 99 ]; then
 		echo "${title##*-} | $line" || exit 1
 	else
 		echo "$title | $line" || exit 1
