8e891253e2a45d9a7571ebac4e88c13bbc484363
Ata Niyazov <ata.niazov@gmail.com>
2017-01-05 01:15:40 +0300

    Added Double-Shift CapsLock toggler

--- beastie/home/config/i3/startup.sh
+++ beastie/home/config/i3/startup.sh
@@ -12,5 +12,5 @@ xfsettingsd &
 xfce4-power-manager &
 xfce4-volumed &
 redshift -l 40.7:29.9 -t 5900:5500 -g 0.8 -m randr &
-compton -cC --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.1 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl-mswc --shadow-exclude fullscreen &
+compton -cCG --shadow-opacity=1.0 --fade-delta=3 --menu-opacity=1.0 --inactive-opacity=1.0 --inactive-dim 0.0 --dbus --detect-rounded-corners --detect-client-opacity --vsync opengl --shadow-exclude fullscreen --frame-opacity=1.0 --no-dnd-shadow --backend glx &
 #conky -c "/home/delta/.conky/SimpleClock/DArk_Simple_" &
